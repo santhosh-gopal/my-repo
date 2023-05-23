8167a4e5978c4bc011b5e4836946f1e2ad8b270a
santhosh-gopal <gopalrutvik@gmail.com>
2023-05-23 16:13:20 +0000

    sbi changes by rishi

--- app.java
+++ app.java
@@ -16,3 +16,18 @@ else if (number == 0) {
 else {
      console.log("The number is negative");
 }
+#sbi changes done by risi @ reports
+// program to generate fibonacci series up to n terms
+
+// take input from the user
+const number = parseInt(prompt('Enter the number of terms: '));
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
